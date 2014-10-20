cf7b4671928229591f77eab9201410630575b34a
Dylan Jones <jonesdyn@gmail.com>
2014-10-20 12:34:11 -0500

    Starting message packing (issue #2)

--- Prototype2/face_pss.c
+++ Prototype2/face_pss.c
@@ -2,8 +2,10 @@
 #include "face_messages.h"
 #include "face_ios.h"
 #include "config_parser.h"
+#include "globals.h"
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,4 +16,23 @@ int main(int argc, char *argv[])
    }
 
    return 0;
+}
+
+#define MAX_BUFF_SIZE 1024
+
+void setDiscrete(int channel, _Bool value)
+{
+   FACE_RETURN_CODE_TYPE retCode;
+   FACE_MESSAGE_LENGTH_TYPE msgLen;
+   char txBuff[MAX_BUFF_SIZE];      // MAX_BUFF_SIZE
+   char rxBuff[MAX_BUFF_SIZE];
+   FACE_IO_MESSAGE_TYPE *txFaceMsg = (FACE_IO_MESSAGE_TYPE*)txBuff;
+   FACE_IO_MESSAGE_TYPE *rxFaceMsg = (FACE_IO_MESSAGE_TYPE*)rxBuff;
+
+   memset(txBuff, 0, MAX_BUFF_SIZE);
+   memset(rxBuff, 0, MAX_BUFF_SIZE);
+}
+
+_Bool readDiscrete(int channel)
+{
 }
